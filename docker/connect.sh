ce5c4e3117248b45efeec0b22d69b5e4ebc74986
Murilo Elias <muriloelias@ipeweb.com.br>
2024-07-05 18:05:53 -0300

    [update] - terminal printing update

--- docker/connect.sh
+++ docker/connect.sh
@@ -3,7 +3,7 @@
 clear
 
 echo -e "
-\t\u001b[30mYou are now in the docker container
+\t\u001b[47m\u001b[30mYou are now in the docker container\u001b[0m
 \t     \u001b[37mUse \u001b[1mexit\u001b[0m \u001b[37mcommand to leave
 ";
 
